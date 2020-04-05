d47aa82f15c0bdd115b17270f1c62141f815abbb
Joe Gray <jgray@advancedpersistentsecurity.net>
2020-04-05 05:24:13 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 apt-get install python3 python3-pip python3-lxml git libre-dev;
-# filepath=`pwd`;
-# git clone https://github.com/google/re2 && cd ./re2;
-# make;
-# make test;
-# make install;
-# make testintall;
-# cd $filepath;
-pip3 install -r requirements.txt;
+pip3 install -U --user -r requirements.txt;
+chmod 755 ./WikiLeaker.py;
+mkdir ~/.recon-ng/modules/pwn;
+cp ./recon-ng_module/wikileaker.py ~/.recon-ng/modules/pwn/wikileaker.py;
