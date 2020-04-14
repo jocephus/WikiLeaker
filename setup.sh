02c3167cb7046fec5a0bab06941ae1691abad2e7
Joe Gray <jgray@advancedpersistentsecurity.net>
2020-04-14 14:51:30 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ apt-get install python3 python3-pip python3-lxml git libre-dev;
 pip3 install -U --user -r requirements.txt;
 chmod 755 ./WikiLeaker.py;
 mkdir ~/.recon-ng/modules/pwn;
-cp ./recon-ng_module/wikileaker.py ~/.recon-ng/modules/pwn/wikileaker.py;
+cp ./recon-ng_module/wikileaker.py ~/.recon-ng/modules/recon/domains-contacts/wikileaker.py;
